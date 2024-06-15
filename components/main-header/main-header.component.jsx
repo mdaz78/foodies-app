d34b5053c75ded6bed9827f24c1f78e96c8735cb
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-15 08:30:00 +0530

    chore: update prettier styling

--- components/main-header/main-header.component.jsx
+++ components/main-header/main-header.component.jsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
-import styles from "./main-header.module.css";
+import styles from './main-header.module.css';
 
-import logo from "@/assets/logo.png";
-import MainHeaderBackground from "../main-header-background/main-header-background.component";
+import logo from '@/assets/logo.png';
+import MainHeaderBackground from '../main-header-background/main-header-background.component';
 
 export default function MainHeader() {
   return (
     <>
       <MainHeaderBackground />
       <header className={styles.header}>
-        <Link href={"/"} className={styles.logo}>
+        <Link href={'/'} className={styles.logo}>
           <Image
             src={logo}
-            alt="Food on a plate"
+            alt='Food on a plate'
             width={100}
             height={100}
             priority
@@ -25,10 +25,10 @@ export default function MainHeader() {
         <nav className={styles.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link href='/meals'>Browse Meals</Link>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <Link href='/community'>Foodies Community</Link>
             </li>
           </ul>
         </nav>
